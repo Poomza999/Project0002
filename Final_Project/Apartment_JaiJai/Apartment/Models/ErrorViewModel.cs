137048fd50b9a31cd025bd17010a07b15bbfac05
poom_555555 <poomza111222@gmail.com>
2026-07-20 23:22:58 +0700

    Fix

--- Final_Project/Apartment_JaiJai/Apartment/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Apartment.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
